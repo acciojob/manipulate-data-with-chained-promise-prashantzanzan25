dc4708cc80138098e13ad3645fb0ab3779cadab2
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-02-04 10:15:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+function manipulateData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 3000);
+  })
+    .then((numbers) => {
+      const evenNumbers = numbers.filter((number) => number % 2 === 0);
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(evenNumbers);
+        }, 1000);
+      });
+    })
+    .then((evenNumbers) => {
+      const multipliedNumbers = evenNumbers.map((number) => number * 2);
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(multipliedNumbers);
+        }, 2000);
+      });
+    })
+    .then((multipliedNumbers) => {
+      const outputElement = document.getElementById("output");
+      outputElement.textContent = multipliedNumbers.join(", ");
+    });
+}
+
+manipulateData();
